0f85468497efa1c1745715a104b185add0a91778
Nagendra Prasad N <prasadpras381@gmail.com>
2021-02-16 10:51:32 +0530

    Turning into PWA app with spinner to load page initialy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { SwPush } from '@angular/service-worker';
+import { Subscription } from 'rxjs';
+import { SplashScreenService } from './shared/_metronic/partials/layout/splash-screen/splash-screen.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'body[root]',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'intensify-now';
+  private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
+
+  constructor(
+    private splashScreenService: SplashScreenService,
+    private router: Router,
+    private swPush: SwPush
+  ) {
+  	/*
+  		// TODO: Firebase & PWA
+	  	// https://medium.com/@a.adendrata/push-notifications-with-angular-6-firebase-cloud-massaging-dbfb5fbc0eeb
+		// https://medium.com/@arjenbrandenburgh/angulars-pwa-swpush-and-swupdate-15a7e5c154ac
+	*/
+    // register translations
+    
+  }
+
+  ngOnInit() {
+  const routerSubscription = this.router.events.subscribe((event) => {
+    if (event instanceof NavigationEnd) {
+      // clear filtration paginations and others
+  
+      // hide splash screen
+      this.splashScreenService.hide();
+
+      // scroll to top on every route change
+      window.scrollTo(0, 0);
+
+      // to display back the body content
+      setTimeout(() => {
+        document.body.classList.add('page-loaded');
+      }, 500);
+    }
+  });
+  this.unsubscribe.push(routerSubscription);
+}
+
+ngOnDestroy() {
+  this.unsubscribe.forEach((sb) => sb.unsubscribe());
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
+import { SplashScreenModule } from './shared/_metronic/partials/layout/splash-screen/splash-screen.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule.forRoot(),
+    SplashScreenModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
