e4a37a8b4c89782fff035560e011b50c25bad29c
Nagendra Prasad N <prasadpras381@gmail.com>
2021-02-17 19:25:17 +0530

    spinner issue resolved

--- src/app/shared/_metronic/partials/layout/splash-screen/splash-screen.service.ts
+++ src/app/shared/_metronic/partials/layout/splash-screen/splash-screen.service.ts
@@ -14,24 +14,24 @@ export class SplashScreenService {
   init(element:ElementRef){
    this.el = element;
   }
-   hide(){
+  hide() {
     if (this.stopped || !this.el) {
       return;
     }
 
-       const dancer = this.animationBuilder.build([style({opacity:'1'}),animate(800, style({opacity:'0'}) )]).create(this.el.nativeElement) 
-   
-       dancer.onDone (() =>{
-         if( typeof this.el.nativeElement.remove === 'function'){
-           this.el.nativeElement.remove()
-         }
-         else{
-          this.el.nativeElement.style.display = 'none !important';
-         }
-         this.stopped = true;
+    const player = this.animationBuilder
+      .build([style({ opacity: '1' }), animate(800, style({ opacity: '0' }))])
+      .create(this.el.nativeElement);
 
-        setTimeout(() => dancer.play(), 100);
-       })
+    player.onDone(() => {
+      if (typeof this.el.nativeElement.remove === 'function') {
+        this.el.nativeElement.remove();
+      } else {
+        this.el.nativeElement.style.display = 'none !important';
       }
+      this.stopped = true;
+    });
 
+    setTimeout(() => player.play(), 100);
+  }
 }
